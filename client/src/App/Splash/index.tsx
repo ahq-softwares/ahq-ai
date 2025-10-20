c6a0eb907d371b02a5f85160ed960e8aac409fe5
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-10-20 20:38:48 +0530

    More fixing

--- client/src/App/Splash/index.tsx
+++ client/src/App/Splash/index.tsx
@@ -2,8 +2,8 @@ import icon from "@/assets/AHQ Progress Logo.png";
 
 export default function Splash() {
   return <div className="w-screen h-screen flex flex-col justify-center text-center items-center">
-    <img src={icon} className="w-[40%] md:w-[20%]" />
+    <img src={icon} className="w-[40%] md:w-[20%] md:max-w-[200px]" />
 
-    <span className="mt-5 dui-loading-xl dui-loading-spinner dui-loading" />
+    <span className="mt-10 dui-loading-spinner dui-loading dui-loading-xl md:!w-[calc(var(--size-selector,0.25rem)*10)]" />
   </div>;
 }
