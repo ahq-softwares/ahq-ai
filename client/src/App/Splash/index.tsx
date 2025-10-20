49c094a3bee2f8a0eeabd14af5f66d3be17ae0fd
YourJailDev <rohanmurudkar122@gmail.com>
2025-10-20 20:30:31 +0530

    finally did something

--- client/src/App/Splash/index.tsx
+++ client/src/App/Splash/index.tsx
@@ -1,4 +1,4 @@
-import icon from "@/assets/react.svg";
+import icon from "@/assets/AHQ AI.png";
 
 export default function Splash() {
   return <div className="w-screen h-screen flex flex-col justify-center text-center items-center">
