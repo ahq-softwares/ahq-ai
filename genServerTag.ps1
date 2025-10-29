1effb37976f16501dc93de515ae7d7a550f50223
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-10-29 08:51:24 +0530

    Try build server

---
+++ genServerTag.ps1
@@ -0,0 +1,18 @@
+Install-Module -Name PSToml -Force -SkipPublisherCheck -AcceptLicense -Scope CurrentUser -ErrorAction Continue
+
+$obj = ConvertFrom-Toml (Get-Content "./server/Cargo.toml")
+
+$version = $obj.package.version
+
+$suffix = Get-Date -Format "yyyy-MM-dd.HH:mm:ss"
+
+$out = $version
+
+if ($env:BRANCH_NAME -eq "main") {
+  $out = "$version-prerelease.$suffix"
+}
+
+if ($env:GITHUB_OUTPUT -ne "") {
+  "tag=server-v$out" >> "$env:GITHUB_OUTPUT"
+  "out=v$out" >> "$env:GITHUB_OUTPUT"
+}
